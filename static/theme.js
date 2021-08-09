5aba6138c8bc250b4f15791b9aeebabb55dfc64d
Alan Garny <agarny@hellix.com>
2021-08-09 15:53:32 +1200

    Documentation: make our copyright function more generic.

--- static/theme.js
+++ static/theme.js
@@ -23,11 +23,13 @@ function resizeCodeElements() {
 // Copyright information
 
 function copyright(relativePath) {
-    var date = new Date();
+    let date = new Date();
+    let startingYear = 2011;
+    let currentYear = date.getFullYear();
 
     document.write("<div class=\"copyright\">\n");
     document.write("    <div class=\"left\">\n");
-    document.write("        Copyright 2011-"+date.getFullYear()+"\n");
+    document.write("        Copyright "+startingYear+((currentYear == startingYear)?"":"-"+currentYear)+"\n");
     document.write("    </div>\n");
     document.write("    <div class=\"center\">\n");
     document.write("        <a class=\"copyright\" href=\"https://twitter.com/TeamOpenCOR/\"><img class=\"twitter\" src=\""+relativePath+"/twitter.png\" width=30 height=30></a>\n");
