ad9ad44dfca80e5934f18b08a17db4b6b5a2fc21
Alan Garny <agarny@hellix.com>
2021-08-09 18:08:35 +1200

    Used CMAKE_CURRENT_XXX_DIR rather than CMAKE_XXX_DIR.

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -1,7 +1,7 @@
 macro(build_documentation DOCUMENTATION_NAME R G B)
     # Make sure that Sphinx is available and that it is at least version 1.4
 
-    list(APPEND CMAKE_MODULE_PATH "${CMAKE_BINARY_DIR}/theme/cmake")
+    list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_BINARY_DIR}/theme/cmake")
 
     if(NOT PYTHON_EXECUTABLE)
         find_package(PythonInterp REQUIRED QUIET)
@@ -28,10 +28,10 @@ macro(build_documentation DOCUMENTATION_NAME R G B)
 
     string(TIMESTAMP SPHINX_YEAR "%Y")
 
-    execute_process(COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_SOURCE_DIR}/src/conf.py
-                                                     ${CMAKE_BINARY_DIR}/conf.py)
+    execute_process(COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_CURRENT_SOURCE_DIR}/src/conf.py
+                                                     ${CMAKE_CURRENT_BINARY_DIR}/conf.py)
 
-    file(APPEND "${CMAKE_BINARY_DIR}/conf.py"
+    file(APPEND "${CMAKE_CURRENT_BINARY_DIR}/conf.py"
         \n
         copyright\ =\ u'2011-${SPHINX_YEAR},\ University\ of\ Auckland'\n
         html_theme\ =\ 'theme'\n
@@ -41,11 +41,11 @@ macro(build_documentation DOCUMENTATION_NAME R G B)
 
     add_custom_target(Sphinx ALL
                       COMMAND ${SPHINX_EXECUTABLE} -q -b html
-                                                   -c "${CMAKE_BINARY_DIR}"
-                                                   "${CMAKE_SOURCE_DIR}/src"
-                                                   "${CMAKE_BINARY_DIR}/html"
-                      COMMAND ${PYTHON_EXECUTABLE} "${CMAKE_BINARY_DIR}/theme/cmake/stringreplace.py"
-                                                   "${CMAKE_BINARY_DIR}/html/_static/theme.css"
+                                                   -c "${CMAKE_CURRENT_BINARY_DIR}"
+                                                   "${CMAKE_CURRENT_SOURCE_DIR}/src"
+                                                   "${CMAKE_CURRENT_BINARY_DIR}/html"
+                      COMMAND ${PYTHON_EXECUTABLE} "${CMAKE_CURRENT_BINARY_DIR}/theme/cmake/stringreplace.py"
+                                                   "${CMAKE_CURRENT_BINARY_DIR}/html/_static/theme.css"
                                                    "103, 103, 103"
                                                    "${R}, ${G}, ${B}"
                       COMMENT "Building the ${DOCUMENTATION_NAME} documentation for OpenCOR")
